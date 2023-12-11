25ddde6fccc4a8df59450d206d759bedf95d9b72
jgm011 <jgm01@proton.me>
2023-12-11 09:34:08 -0300

    Advancement in design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,68 +16,73 @@ export default function Footer(props: PropsLayout) {
   return (
     <div className="footer">
       <Container parentClass="footer">
-        <div className="footer__images">
-          <img
-            src={FooterLogo}
-            alt="Footer Logo"
-            className="footer__images__logo"
-          />
-          <div className="footer__images__icons-box">
-          <a href="#">
+        <div className="footer__first-block">
+          <div className="footer__first-block__images">
             <img
-              src={FacebookIcon}
-              alt="Facebook Icon"
-              className="footer__images__icons-box__social-media-icon"
+              src={FooterLogo}
+              alt="Footer Logo"
+              className="footer__first-block__images__logo"
             />
-          </a>
-          <a href="#">
-            <img
-              src={YoutubeIcon}
-              alt="Youtube Icon"
-              className="footer__images__icons-box__social-media-icon"
-            />
-          </a>
-          <a href="#">
-            <img
-              src={TwitterIcon}
-              alt="Twitter Icon"
-              className="footer__images__icons-box__social-media-icon"
-            />
-          </a>
-          <a href="#">
-            <img
-              src={PinterestIcon}
-              alt="Pinterest Icon"
-              className="footer__images__icons-box__social-media-icon"
-            />
-          </a>
-          <a href="#">
-            <img
-              src={InstagramIcon}
-              alt="Instagram Icon"
-              className="footer__images__icons-box__social-media-icon"
-              style={{ fill: "green" }}
-            />
-          </a></div>
+            <div className="footer__first-block__images__icons-box">
+              <a href="#">
+                <img
+                  src={FacebookIcon}
+                  alt="Facebook Icon"
+                  className="footer__first-block__images__icons-box__social-media-icon"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src={YoutubeIcon}
+                  alt="Youtube Icon"
+                  className="footer__first-block__images__icons-box__social-media-icon"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src={TwitterIcon}
+                  alt="Twitter Icon"
+                  className="footer__first-block__images__icons-box__social-media-icon"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src={PinterestIcon}
+                  alt="Pinterest Icon"
+                  className="footer__first-block__images__icons-box__social-media-icon"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src={InstagramIcon}
+                  alt="Instagram Icon"
+                  className="footer__first-block__images__icons-box__social-media-icon"
+                  style={{ fill: "green" }}
+                />
+              </a>
+            </div>
+          </div>
+          <nav className="footer__first-block__navbar">
+            <ul className="footer__first-block__navbar__list">
+              {props.items.map((item, index) => (
+                <li
+                  className="footer__first-block__navbar__list__item"
+                  key={`${item}-${index}`}
+                >
+                  <a href="#" className="footer__first-block__navbar__list__item__link">
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
-        <nav className="footer__navbar">
-          <ul className="footer__navbar__list">
-            {props.items.map((item, index) => (
-              <li
-                className="footer__navbar__list__item"
-                key={`${item}-${index}`}
-              >
-                <a href="#" className="footer__navbar__list__item__link">
-                  {item}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="footer__last-box">
-          {props.button}
-          <div className="footer__last-box__copyright">
-            © Easybank. All Rights Reserved
+        <div className="footer__second-block">
+          <div className="footer__second-block__last-box">
+            {props.button}
+            <div className="footer__second-block__last-box__copyright">
+              © Easybank. All Rights Reserved
+            </div>
           </div>
         </div>
       </Container>
